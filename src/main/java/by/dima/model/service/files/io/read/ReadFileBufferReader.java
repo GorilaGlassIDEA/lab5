69e54d679c1467ab89cb430955526fa64e8c9ce7
dmitrijmartynov <dotaxai@gmail.com>
2025-03-14 11:18:18 +0300

    fix formatting code

--- src/main/java/by/dima/model/service/files/io/read/ReadFileBufferReader.java
+++ src/main/java/by/dima/model/service/files/io/read/ReadFileBufferReader.java
@@ -1,7 +1,6 @@
 package by.dima.model.service.files.io.read;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -18,7 +17,7 @@ public class ReadFileBufferReader implements ReadableFile {
 
     @Override
     public String getContent() {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String newLine;
             while ((newLine = bufferedReader.readLine()) != null) {
                 contentFromPath += newLine + "\n";
