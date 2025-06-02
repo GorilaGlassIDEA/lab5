8b5b762d6d81638b2b3e9fc03ccc10fe43b7f65a
dmitrijmartynov <dotaxai@gmail.com>
2025-06-02 10:31:28 +0300

    init

--- src/main/java/by/dima/model/common/UserModel.java
+++ src/main/java/by/dima/model/common/UserModel.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Data
@@ -13,6 +14,8 @@ public final class UserModel implements Serializable {
     private Integer id;
     private String username;
     private String password;
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     /**
      * Конструктор для авторизации
